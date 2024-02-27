7a7b737fe8894686c3fc9730152abd0a04691ce7
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-02-27 23:08:48 +0530

    fix: validation logics were added

--- ShoppingList/lib/widgets/new_item.dart
+++ ShoppingList/lib/widgets/new_item.dart
@@ -28,7 +28,13 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  return 'Demo...';
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50){
+                        return 'Must be between 1 and 50 characters';
+                      } 
+                      return null;
                 },
               ), // instead of TextField()
               Row(
@@ -40,6 +46,16 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      int.tryParse(value) == null ||
+                      int.tryParse(value)! <= 0
+                      ){
+                        return 'Must be a valid positive number.';
+                      } 
+                      return null;
+                },
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -64,6 +80,22 @@ class _NewItemState extends State<NewItem> {
                       ],
                       onChanged: (value) {},
                     ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add Item'),
                   ),
                 ],
               ),
