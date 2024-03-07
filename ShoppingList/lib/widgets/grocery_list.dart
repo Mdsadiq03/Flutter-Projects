3de66f4031f587ac670e1ad56d9dea0670562842
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-03-07 21:18:59 +0530

    fix: Avoiding unnessesary request

--- ShoppingList/lib/widgets/grocery_list.dart
+++ ShoppingList/lib/widgets/grocery_list.dart
@@ -20,36 +20,48 @@ class _GroceryListState extends State<GroceryList> {
   @override
   void initState() {
     super.initState();
-    _load_items();
+    _loadItems();
   }
 
-  void _load_items() async {
+  void _loadItems() async {
     final url = Uri.https(
         'shopping-list-1a-default-rtdb.firebaseio.com', 'Shopping-List.json');
     final response = await http.get(url);
     final Map<String, dynamic> listData =
         json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
+    final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
       final category = categories.entries.firstWhere(
           (catItem) => catItem.value.title == item.value['category']).value;
 
-      _loadedItems.add(GroceryItem(
+      loadedItems.add(GroceryItem(
           id: item.key,
           name: item.value['name'],
           quantity: item.value['quantity'],
           category: category));
     }
     setState((){
-     _groceryItems = _loadedItems;  
+     _groceryItems = loadedItems;  
     });
     
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+   final newItem = await Navigator.of(context).push<GroceryItem>(
         MaterialPageRoute(builder: (ctx) => const NewItem()));
+  
+
+  if (newItem == null){
+    return;
   }
+
+  setState(() {
+    _groceryItems.add(newItem);
+  });
+
+  }
+
+
 
   void _removeItem(GroceryItem item) {
     setState(() {
