77f755a9141cc0a3b9769c4998ed5f266d61d634
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-03-20 20:31:55 +0530

    Fix: Added http request url

--- Favoriteplaces/widgets/location_input.dart
+++ Favoriteplaces/widgets/location_input.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -42,13 +45,20 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    final url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyCp4r0qio4-nO_uWGrUCFBJGCiMMsgyIbE');
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final address = resData['result'][0]['formatted_address'];
+
 
     setState(() {
       _isGettingLocation = false;
     });
 
-    print(locationData.latitude);
-    print(locationData.longitude);
+    
   }
 
   @override
