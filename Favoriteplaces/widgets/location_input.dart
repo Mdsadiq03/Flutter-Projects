138bc359bd90c50c223617197bb21631a10323a3
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-03-20 20:44:00 +0530

    Fix: Added PlaceLocation property

--- Favoriteplaces/widgets/location_input.dart
+++ Favoriteplaces/widgets/location_input.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -14,7 +15,7 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
   void _getCurrentLocation() async {
@@ -48,6 +49,10 @@ class _LocationInputState extends State<LocationInput> {
     final lat = locationData.latitude;
     final lng = locationData.longitude;
 
+    if (lat == null || lng == null){
+      return;
+    }
+
     final url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyCp4r0qio4-nO_uWGrUCFBJGCiMMsgyIbE');
     final response = await http.get(url);
     final resData = json.decode(response.body);
@@ -55,6 +60,7 @@ class _LocationInputState extends State<LocationInput> {
 
 
     setState(() {
+      _pickedLocation = PlaceLocation(latitude: lat, longitude: lng, address: address,);
       _isGettingLocation = false;
     });
 
