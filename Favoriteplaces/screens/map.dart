b346ecd0248368978a4a1d20cbb6a1c0b91c61f7
Mdsadiq03 <mohamedsadiq2k3@gmail.com>
2024-03-24 11:37:13 +0530

    Fix: Handling taps for selecting location

--- Favoriteplaces/screens/map.dart
+++ Favoriteplaces/screens/map.dart
@@ -23,6 +23,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,14 +40,22 @@ class _MapScreenState extends State<MapScreen> {
         ],
       ),
       body: GoogleMap(
+        onTap: (position) {
+          setState(() {
+            _pickedLocation = position;
+          });
+        },
         initialCameraPosition: CameraPosition(
-          target: LatLng(widget.location.latitude, widget.location.longitude ),
+          target: LatLng(widget.location.latitude, widget.location.longitude),
           zoom: 16,
         ),
-        markers: {
+        markers: (_pickedLocation == null && widget.isSelecting) ? {} : {
           Marker(
             markerId: const MarkerId('m1'),
-            position: LatLng(widget.location.latitude, widget.location.longitude)
+            position: _pickedLocation ?? LatLng(
+                    widget.location.latitude,
+                    widget.location.longitude,
+                  ),
           ),
         },
       ),
